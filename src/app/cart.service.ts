a4358f84de6ac8e50280f89bc4ca26abf41232e5
jonolds <jonolds@gmail.com>
2019-06-25 03:22:39 -0500

    win

---
+++ src/app/cart.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+  items = [];
+
+  addToCart(product) {
+    this.items.push(product);
+    window.alert('ADD TO CART SERVICE');
+  }
+
+  getItems() {
+    return this.items;
+  }
+
+  clearCart() {
+    this.items = [];
+    return this.items;
+  }
+}
